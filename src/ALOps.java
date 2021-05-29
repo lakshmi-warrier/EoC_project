60632da6eb9330c7c13baf423bafb1c1e1f8c77a
lakshmi-warrier <lakshmi0105warrier@gmail.com>
2021-05-29 19:21:29 +0530

    Refactor: ALOps - SB

--- src/ALOps.java
+++ src/ALOps.java
@@ -3,59 +3,107 @@ public class ALOps {
 
     static int compNum = 0;
 
-    static String add() {
-        return "\n//add\n@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "M=D+M\n" + "@SP\n" + "M=M+1";
+    static String ops(String op) {
+        switch (op) {
+        case "add":
+            return ops1(op, "M=D+M\n");
+
+        case "sub":
+            return ops1(op, "M=M-D\n");
+
+        case "or":
+            return ops1(op, "M=D|M\n");
+        case "and":
+            return ops1(op, "M=M&D\n");
+
+        case "neg":
+            return ops2(op, "M=-M\n");
+
+        case "not":
+            return ops2(op, "M=!M\n");
+
+        case "eq":
+            return ops3(op, "D;JEQ\n");
+        case "gt":
+            return ops3(op, "D;JGT\n");
+        case "lt":
+            return ops3(op, "D;JLT\n");
+        case "return":
+            return Return();
+        default:
+            return "";
+        }
     }
 
-    static String sub() {
-        return "\n//sub\n@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "M=M-D\n" + "@SP\n" + "M=M+1";
+    static String ops1(String op, String code) {
+        // HANDLE AND, OR, ADD, SUB
+        StringBuilder sb = new StringBuilder();
 
+        sb.append("\n//" + op);
+        sb.append("\n@SP");
+        sb.append("\nAM=M-1");
+        sb.append("\nD=M\n");
+        sb.append("@SP\n");
+        sb.append("AM=M-1\n");
+        sb.append(code);
+        sb.append("@SP\n");
+        sb.append("M=M+1");
+
+        return sb.toString();
     }
 
-    static String or() {
-        return "\n//or\n@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "M=D|M\n" + "@SP\n" + "M=M+1";
+    static String ops2(String op, String code) {
+        // HANDLES NOT, NEG
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\n//" + op + "\n");
+        sb.append("@SP\n");
+        sb.append("AM=M-1\n");
+        sb.append(code);
+        sb.append("@SP\n");
+        sb.append("M=M+1");
+
+        return sb.toString();
     }
 
-    static String and() {
-        return "\n//and\n@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\nAM=M-1\n" + "M=M&D\n" + "@SP\nM=M+1";
-    }
-
-    static String gt() {
+    static String ops3(String op, String code) {
+        // HANDLES LT, GT, EQ
         compNum++;
-        return "\n//gt\n" + "@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=M-D\n" + "@TRUE" + compNum + "\n"
-                + "D;JGT\n" + "@FALSE" + compNum + "\n" + "0;JMP\n" + "(TRUE" + compNum + ")\n" + "@SP\n" + "A=M\n"
-                + "M=-1\n" + "@SP\n" + "M=M+1\n" + "@END" + compNum + "\n" + "0;JMP\n" + "(FALSE" + compNum + ")\n"
-                + "@SP\n" + "A=M\n" + "M=0\n" + "@SP\n" + "M=M+1\n" + "(END" + compNum + ")";
-    }
+        StringBuilder sb = new StringBuilder();
 
-    static String lt() {
-        compNum++;
-        return "\n//lt\n" + "@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=M-D\n" + "@TRUE" + compNum + "\n"
-                + "D;JLT\n" + "@FALSE" + compNum + "\n" + "0;JMP\n" + "(TRUE" + compNum + ")\n" + "@SP\n" + "A=M\n"
-                + "M=-1\n" + "@SP\n" + "M=M+1\n" + "@END" + compNum + "\n" + "0;JMP\n" + "(FALSE" + compNum + ")\n"
-                + "@SP\n" + "A=M\n" + "M=0\n" + "@SP\n" + "M=M+1\n" + "(END" + compNum + ")";
-    }
+        sb.append("\n//" + op + "\n");
+        sb.append("AM=M-1\n");
+        sb.append("D=M\n");
+        sb.append("@SP\n");
+        sb.append("AM=M-1\n");
+        sb.append("D=M-D\n");
+        sb.append("@TRUE" + compNum + "\n");
+        sb.append(code);
+        sb.append("@FALSE" + compNum + "\n");
+        sb.append("0;JMP\n");
+        sb.append("(TRUE" + compNum + ")\n");
+        sb.append("@SP\n");
+        sb.append("A=M\n");
+        sb.append("M=-1\n");
+        sb.append("@SP\n");
+        sb.append("M=M+1\n");
+        sb.append("@END");
+        sb.append(compNum + "\n");
+        sb.append("0;JMP\n");
+        sb.append("(FALSE" + compNum + ")\n");
+        sb.append("@SP\n");
+        sb.append("A=M\n");
+        sb.append("M=0\n");
+        sb.append("@SP\n");
+        sb.append("M=M+1\n");
+        sb.append("(END" + compNum + ")");
 
-    static String neg() {
-        return "\n//neg\n" + "@SP\n" + "AM=M-1\n" + "M=-M\n" + "@SP\n" + "M=M+1";
-    }
-
-    static String not() {
-        return "\n//not\n" + "@SP\n" + "AM=M-1\n" + "M=!M\n" + "@SP\n" + "M=M+1";
-    }
-
-    static String eq() {
-        compNum++;
-        return "\n//eq\n@SP\n" + "AM=M-1\n" + "D=M\n" + "@SP\n" + "AM=M-1\n" + "D=M-D\n" + "@TRUE" + compNum + "\n"
-                + "D;JEQ\n" + "@FALSE" + compNum + "\n" + "0;JMP\n" + "(TRUE" + compNum + ")\n" + "@SP\n" + "A=M\n"
-                + "M=-1\n" + "@SP\n" + "M=M+1\n" + "@END" + compNum + "\n" + "0;JMP\n" + "(FALSE" + compNum + ")\n"
-                + "@SP\n" + "A=M\n" + "M=0\n" + "@SP\n" + "M=M+1\n" + "(END" + compNum + ")";
-
+        return sb.toString();
     }
 
     static String function(String f, int k) {
         String s = "//" + f + "\n(" + f + ")\n" + "@SP\n" + "A=M\n";
-        for (int i = 0; i < k; i ++) {
+        for (int i = 0; i < k; i++) {
             s += "M=0\n" + "A=A+1\n";
         }
         return s + "D=A\n" + "@SP\n" + "M=D\n";
---
+++ src/JackTokenizer.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class JackTokenizer
+{
+    File infile;
+    JackTokenizer(File inFile)
+    {
+        this.infile = inFile;
+    }
+    hasMoreTokens()
+    advance()
+    tokenType()
+    keyword()
+    symbol()
+    identifier()
+    intVal()
+    stringVal()
+}
