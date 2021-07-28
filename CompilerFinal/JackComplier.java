a2695883bc79e8ed716b69b2da795695ca5d1e9c
Devika Manoj <dmenon138@gmail.com>
2021-07-28 10:23:46 +0530

    comments are added

--- CompilerFinal/JackComplier.java
+++ CompilerFinal/JackComplier.java
@@ -2,51 +2,33 @@ import java.io.*;
 import java.util.*;
 public class JackComplier 
 {
-    public static ArrayList<File> getJackFiles(File dir)
-    {
-        File[] files = dir.listFiles();
-        ArrayList<File> result = new ArrayList<File>();
-        if (files == null) return result;
-
-        for (File f:files)
-        {
-            if (f.getName().endsWith(".jack"))
-            {
-                result.add(f);
-            }
-        }
-        return result;
-    }
     public static void main(String[] args) 
     {
-        System.out.print("Enter file name: ");
+        System.out.print("Enter the path of the folder: ");
         Scanner in = new Scanner(System.in);
-        String fileInName = in.next();
+        String fileInName = in.next(); // folder path is taken as a string
         File fileIn = new File(fileInName);
 
         String fileOutPath = "";
 
         File fileOut;
 
-        ArrayList<File> jackFiles = new ArrayList<File>();
+        ArrayList<File> jackFiles = new ArrayList<File>(); // adding the file name to arraylist
 
-        if (fileIn.isFile()) 
+        if (fileIn.isFile()) // If a file path is entered
         {
-            //if it is a single file, see whether it is a vm file
             String path = fileIn.getAbsolutePath();
-            if (!path.endsWith(".jack")) 
+            if (!path.endsWith(".jack")) // checks if the path entered ends with .jack
             {
                 System.out.println("JACK FILE NOT PRESENT");
             }
-            jackFiles.add(fileIn);
+            jackFiles.add(fileIn); // if the path ends with .jack it gets added onto the array list
         } 
-        else if (fileIn.isDirectory()) 
+        else if (fileIn.isDirectory()) //if it is a directory get all jack files under this directory
         {
-            //if it is a directory get all jack files under this directory
-            jackFiles = getJackFiles(fileIn);
+            jackFiles = getJackFiles(fileIn); // goes to the method getJackFiles
 
-            //if no  file in this directory
-            if (jackFiles.size() == 0) 
+            if (jackFiles.size() == 0) //if no  file in this directory
             {
                 System.out.println(" NO JACK FILE FOUND");
             }
@@ -57,13 +39,32 @@ public class JackComplier
         }
         for (File f: jackFiles) 
         {
-            fileOutPath = f.getAbsolutePath().substring(0, f.getAbsolutePath().lastIndexOf(".")) + ".vm";
+            // creates a vm file where the folder where the .jack is being stored
+            fileOutPath = f.getAbsolutePath().substring(0, f.getAbsolutePath().lastIndexOf(".")) + ".vm"; 
             fileOut = new File(fileOutPath);
 
             CompilationEngine compilationEngine = new CompilationEngine(f,fileOut);
-            compilationEngine.ClassCompiler();
+            compilationEngine.ClassCompiler(); // goes to the method ClassCompiler in the class compilation Engine
 
-            System.out.println(" File is created in : " + fileOutPath);
+            System.out.println(" File is created in : " + fileOutPath); // file path where the vm file generated is being printed
         }
+    }
+    // to get the jack files from the folder
+    public static ArrayList<File> getJackFiles(File dir)
+    {
+        File[] files = dir.listFiles();
+        ArrayList<File> result = new ArrayList<File>(); // an array list is created to add jack files
+        if (files == null) // checks if the folder is null
+        {
+            return result; 
+        }
+        for (File f:files)
+        {
+            if (f.getName().endsWith(".jack"))
+            {
+                result.add(f); // if the files in the folder ends with .jack, it gets added the array list
+            }
+        }
+        return result;
     }
 }
