fc002f6b7c81ce609f3d3c6dbbfb7c288b57d6af
Lakshmi Warrier <lakshmi0105warrier@gmail.com>
2021-07-29 11:21:11 +0530

    Delete Symbols.java, updated VM files (#6)
    Deleted Symbol class, added VM output of our compiler

--- CompilerFinal/SymbolTable.java
+++ CompilerFinal/SymbolTable.java
@@ -1,4 +1,44 @@
 import java.util.*;
+
+class Symbol 
+{
+    public static enum KIND 
+    {
+        STATIC, FIELD, ARG, VAR, NONE
+    };
+
+    String type;
+    KIND kind;
+    int index;
+
+    public Symbol(String type, KIND kind, int index) 
+    {
+        this.type = type;
+        this.kind = kind;
+        this.index = index;
+    }
+
+    public String getType() 
+    {
+        return type;
+    }
+
+    public KIND getKind() 
+    {
+        return kind;
+    }
+
+    public int getIndex() 
+    {
+        return index;
+    }
+
+    @Override
+    public String toString() {
+        return "Symbol{" + "type='" + type + '\'' + ", kind=" + kind + ", index=" + index + '}';
+    }
+}
+
 public class SymbolTable 
 {
     static HashMap<String,Symbol> classSymbols;//for STATIC, FIELD
